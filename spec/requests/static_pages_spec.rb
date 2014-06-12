971a90218e2bfe7ee6b2db95fcfa4fb135bc463f
Serega8 <intellectwork@gamil.com>
2014-06-12 12:04:50 +0300

    Contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,13 +40,17 @@ describe "Static pages" do
 
    describe "Contact page" do
 
-    it "should have the content 'Contact Us'" do
+    it "should have the content 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_content('Contact Us')
+      expect(page).to have_content('Contact')
     end
-    it "should have the title 'Contact Us'" do
+    it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
+
+
+
+
 end
